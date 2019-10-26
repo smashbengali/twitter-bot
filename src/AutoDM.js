7829e4cec372e3895587fe3e899557c5354145eb
smashbengali <56982939+smashbengali@users.noreply.github.com>
2019-10-26 01:04:18 +0100

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Yo yo yooo whattup ${name}! Just wanted to message and personally thank you for following and joining the gushti! \n If possible, i'd really appreciate if you could support by subscribing if you haven't done already - www.youtube.com/smashbengali. Huge love! 💯 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
